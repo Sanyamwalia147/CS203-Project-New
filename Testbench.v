a4b523187cc79555f157595ef8624f3bb46ff783
Sanyamwalia147 <93070460+Sanyamwalia147@users.noreply.github.com>
2021-12-02 12:48:14 +0530

    Added testbench (part 3)

--- Testbench.v
+++ Testbench.v
@@ -125,3 +125,49 @@ clk=1'b0;
      $display("State: RIGHT PASSWORD\n");
    else if(state==3'b100)
      $display("State: STOP\n");
+    
+  clk=1'b0;
+ reset_n = 0;
+ sensor_entrance = 0;
+ sensor_exit = 1;
+ password = 2'b11;
+ #10 clk=1'b1;
+ #10;
+   $display("\nINPUTS\nSensor-entrance: %b, Sensor-exit: %b",sensor_entrance, sensor_exit);
+   $display("Password: %b  (Correct password=11)",password);
+   $display("OUTPUTS\nRed Light: %b, Green Light: %b",RED_LED, GREEN_LED);
+   
+   $display("Number: %b(Binary), %d (Decimal) | Fare: %d(Decimal)",number,number,fare);
+   if(state==3'b000)
+     $display("State: IDLE\n");
+   else if(state==3'b001)
+     $display("State: WAIT PASSWORD\n");
+   else if(state==3'b010)
+     $display("State: WRONG PASSWORD\n");
+   else if(state==3'b011)
+     $display("State: RIGHT PASSWORD\n");
+   else if(state==3'b100)
+     $display("State: STOP\n");
+     
+ clk=1'b0;
+ reset_n = 0;
+ sensor_entrance = 1;
+ sensor_exit = 0;
+ password = 2'b00;
+ #10 clk=1'b1;
+ #10;
+   $display("\nINPUTS\nSensor-entrance: %b, Sensor-exit: %b    //New Car incoming. Red light and ask for password.",sensor_entrance, sensor_exit);
+   $display("Password: %b  (Correct password=11)",password);
+   $display("OUTPUTS\nRed Light: %b, Green Light: %b",RED_LED, GREEN_LED);
+   
+   $display("Number: %b(Binary), %d (Decimal) | Fare: %d(Decimal)",number,number,fare);
+   if(state==3'b000)
+     $display("State: IDLE\n");
+   else if(state==3'b001)
+     $display("State: WAIT PASSWORD\n");  
+   else if(state==3'b010)
+     $display("State: WRONG PASSWORD\n");
+   else if(state==3'b011)
+     $display("State: RIGHT PASSWORD\n");
+   else if(state==3'b100)
+     $display("State: STOP\n");
