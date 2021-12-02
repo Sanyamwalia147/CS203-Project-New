c6f2c866119f4a2d3d8b61a3bda218eeae41e651
rohitkinha <87006871+rohitkinha@users.noreply.github.com>
2021-12-02 12:31:56 +0530

    Added the state interconnection part

--- module.v
+++ module.v
@@ -11,7 +11,7 @@ module parking_system(
  );
  parameter IDLE = 3'b000, WAIT_PASSWORD=3'b001, WRONG_PASS=3'b010, RIGHT_PASS=3'b011, STOP = 3'b100;
  reg[2:0] current_state, next_state;
- //reg[31:0] counter_wait;
+
  reg red_tmp,green_tmp;
   
   initial begin
@@ -19,3 +19,49 @@ module parking_system(
    number=4'b0000;
    fare=10'b0000000000;
   end
+//State Interconnections...
+ always @(*)
+   begin
+    case(current_state)
+     IDLE: 
+       begin
+        if(sensor_entrance==1 && sensor_exit==1)
+ 		 next_state = STOP;
+   		else if(sensor_entrance==1)
+ 		 next_state = WAIT_PASSWORD;
+   		else
+     	 next_state = IDLE;
+ 	   end
+     WAIT_PASSWORD: 
+       begin
+          if((password==2'b11))
+		   next_state = RIGHT_PASS;
+		  else
+		   next_state = WRONG_PASS;
+	   end
+ 	 WRONG_PASS: 
+       begin
+        if(password==2'b11)
+ 		 next_state = RIGHT_PASS;
+ 		else
+		 next_state = WRONG_PASS;
+	   end
+     RIGHT_PASS: 
+       begin
+		if(sensor_entrance==1 && sensor_exit==1)
+		 next_state = STOP;
+		else if(sensor_exit==1)
+		 next_state = IDLE;
+		else
+		 next_state = RIGHT_PASS;
+		end
+     STOP: 
+       begin
+		 if(sensor_entrance==1 && sensor_exit==1)
+		  next_state = STOP;
+		 else
+ 		  next_state = WAIT_PASSWORD;
+ 	   end
+     default: next_state = IDLE;
+    endcase
+   end
